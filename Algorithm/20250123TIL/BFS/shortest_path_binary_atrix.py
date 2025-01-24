b9747c545b062a9347eb5f2454d1c5dea4cf8789
Ansu-dev <ansu1007@naver.com>
2025-01-24 13:01:18 +0900

    📝 Add: shortest_path

--- Algorithm/20250123TIL/BFS/shortest_path_binary_atrix.py
+++ Algorithm/20250123TIL/BFS/shortest_path_binary_atrix.py
@@ -27,23 +27,35 @@ grid2 = [
     [1, 1, 0],
 ]
 
-directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+directions = [(-1, 0), (1, 0), (0, -1), (0, 1), (-1, -1), (-1, 1), (1, 1), (1, -1)]
 
 def bfs(grid, visited):
-    queue = deque([(0, 0)])
+    path_length = -1
+
+    queue = deque([(0, 0, 1)])
     visited[0][0] = True # 첫번째 노드는 방문
     while queue:
-        x, y = queue.popleft()
-        if grid[x][y] == 1:
-            return -1
+        x, y, l = queue.popleft()
+        # 목적지에 도착했을 때 그때의 path_lenth에 저장하면 됨
+        if x == len(grid) - 1 and y == len(grid[0]) - 1:
+            path_length = l # 가장 빨리 끝나는 length가 path_length에 담김
+            break
+
+        for dx,dy in directions:
+            new_dx, new_dy = dx + x, dy + y
+            if 0 <= new_dx < len(grid) and 0 <= new_dy < len(grid[0]) and not visited[new_dx][new_dy] and grid[new_dx][new_dy] == 0:
+                queue.append((new_dx, new_dy, l + 1)) # 길이 정보도 함께 넣어 준다(최단 경로를 구하기 위해서)
+                visited[new_dx][new_dy] = True
+
+    return path_length
 
 
 
 def solution(grid):
     visited = [[False] * len(grid[0]) for _ in range(len(grid))]
-
-    count = bfs(grid, visited)
-    return count
+    if grid[0][0] == 1 or gird[len(grid) - 1][len(grid[0] - 1)] == 1: # 시작이1이거나 끝지점이 1인 경우는 -1 반환
+        return -1
+    return bfs(grid, visited)
 
 
 print(solution(grid1)) # 4
