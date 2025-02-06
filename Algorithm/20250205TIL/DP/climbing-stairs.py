5e200ff7d9b53db211f4cecac3e871daaabb4c70
Ansu-dev <ansu1007@naver.com>
2025-02-06 15:17:43 +0900

    📝 Add: climbing-stairs

--- Algorithm/20250205TIL/DP/climbing-stairs.py
+++ Algorithm/20250205TIL/DP/climbing-stairs.py
@@ -3,10 +3,35 @@
 
 # 1 <= n  45
 
-# 1걸음 또는 2걸음 걸음수 있음 총 경우의 수는 몇 가지?
-def solution(n):
-    # 1과 2를 더해 n을 만들수 있는 조합
-    return
+# 접근 방법 => 완전탐색
+# 1. 크고 복잡한 문제를 하위 문제로 나눈다.
+# 2. 하위 문제에 대한 답을 계산한다.
+# 3. 하위 문제에 대한 답으로 원래 문제에 대한 답을 계산한다.
 
-print(solution(2)) # 2 -> 1step + 1step, 2 steps
-print(solution(3)) # 3 -> 1 step + 1step + 1step, 1step + 2steps, 2steps + 1step
+# 메모이제이션
+memo = {}
+
+# 1걸음 또는 2걸음 걸음수 있음 총 경우의 수는 몇 가지?
+# n으로 도달하려면
+# n - 1, n - 2 에서 와야한다.
+# n - 1, n - 2에 도달할 수 있는 모든 경우의를 합친것
+def cs(n):
+    ## top-down 방식
+    if n == 1:
+        return 1
+    if n == 2:
+        return 2
+    if n not in memo:
+        memo[n] = cs(n-1)+cs(n-2)
+
+    ## bottom-up 방식
+    memo[1] = 1
+    memo[2] = 2
+    for i in range(3, n+1):
+        memo[i] = memo[i-1] + memo[i-2]
+
+    return memo[n]
+
+
+print(cs(2)) # 2 -> 1step + 1step, 2 steps
+print(cs(3)) # 3 -> 1 step + 1step + 1step, 1step + 2steps, 2steps + 1step
