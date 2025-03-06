1b5a6c0f87d3f462f44389102a5aa6f200ca6642
Ansu-dev <ansu1007@naver.com>
2025-03-06 11:47:07 +0900

    ✅ Update: 후보키 combination 버전

--- Algorithm/20250305TIL/42890.py
+++ Algorithm/20250305TIL/42890.py
@@ -12,12 +12,28 @@ def solution(relation):
     answer = 0
     columns = [i for i, _ in enumerate(relation[0])] # 컬럼 8이하
     # 컬럼의 모든 조합을 만듬
+    found_keys = [] # 후보키 목록
     def backtracking(start,current):
         nonlocal answer
         if current: # 빈 조합은 제외하고 저장
-            print(current)
-            # 후보키를 판별
-
+            # 1. 유일성 체크
+            seen = set() # 유일성 검사를 위해 중복이 없는 튜플들을 저장할 set()을 생성
+            for row in relation:
+                key = tuple(row[i] for i in current) # 현재 선택된 컬럼 조합으로 튜플 생성
+                seen.add(key) # relation에서 조합된 튜플의 key를 기준으로 값을 모두 넣음
+            if len(seen) == len(relation): # 중복이 없으면 유일성 만족 -> 해당 tuple의 Key를 기준으로 모든 row의 개수가 relation의 개수와 동일할 시
+                # 2. 최소성 체크
+                is_minimal = True
+                for key in found_keys:
+                    # set(A).issubset(set(B)) -> 집합A가 집합 B의 부분집합이면 True 반환
+                    if set(key).issubset(set(current)): # 기존 후보키(key)가 현재 컬럼 조합(current)의 부분집합이면 현재 조합은 최소성을 위배
+                        is_minimal = False
+                        break # 현재 for문을 중단
+                # 최소성을 만족할 경우에만
+                if is_minimal:
+                    # 후보키로 인정
+                    found_keys.append(current[:]) # 리스트 복사
+                    answer += 1
 
         for i in range(start, len(columns)):
             current.append(columns[i])
